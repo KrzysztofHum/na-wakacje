50b11b8aaa05ff931ee09bbb5c2f30c4bfcdafdd
KrzysztofHum <k.humienny1996@protonmail.com>
2024-07-19 14:49:01 +0200

    e2e my hotels

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from "@playwright/test";
 import path from "path";
 
-const UI_URI = "http://localhost:5173/";
+const UI_URL = "http://localhost:5173/";
 test.beforeEach(async ({ page }) => {
-  await page.goto(UI_URI);
+  await page.goto(UI_URL);
   //get the sign in button
   await page.getByRole("link", { name: "Zaloguj" }).click();
   await expect(page.getByRole("heading", { name: "Zaloguj" })).toBeVisible();
@@ -16,7 +16,7 @@ test.beforeEach(async ({ page }) => {
 });
 
 test("should allow user to add a hotel", async ({ page }) => {
-  await page.goto(`${UI_URI}add-hotel`);
+  await page.goto(`${UI_URL}add-hotel`);
   await page.locator('[name="name"]').fill("Test Hotel");
   await page.locator('[name="city"]').fill("Test City");
   await page.locator('[name="country"]').fill("Test Country");
@@ -38,4 +38,41 @@ test("should allow user to add a hotel", async ({ page }) => {
   ]);
   await page.getByRole("button", { name: "Zapisz" }).click();
   await expect(page.getByText("Hotel Zapisany!")).toBeVisible();
+});
+
+test("should display hotels", async ({ page }) => {
+  await page.goto(`${UI_URL}my-hotels`);
+
+  await expect(page.getByText("Dublin Getaways")).toBeVisible();
+  await expect(page.getByText("Lorem ipsum dolor sit amet")).toBeVisible();
+  await expect(page.getByText("Dublin, Ireland")).toBeVisible();
+  await expect(page.getByText("All Inclusive")).toBeVisible();
+  await expect(page.getByText("£119 per night")).toBeVisible();
+  await expect(page.getByText("2 adults, 3 children")).toBeVisible();
+  await expect(page.getByText("2 Star Rating")).toBeVisible();
+
+  await expect(
+    page.getByRole("link", { name: "View Details" }).first()
+  ).toBeVisible();
+  await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
+});
+
+test("should edit hotel", async ({ page }) => {
+  await page.goto(`${UI_URL}my-hotels`);
+
+  await page.getByRole("link", { name: "Zobacz szczegóły" }).first().click();
+
+  await page.waitForSelector('[name="name"]', { state: "attached" });
+  await expect(page.locator('[name="name"]')).toHaveValue("Dublin Getaways");
+  await page.locator('[name="name"]').fill("Dublin Getaways UPDATED");
+  await page.getByRole("button", { name: "Zapisz" }).click();
+  await expect(page.getByText("Hotel Zapisany!")).toBeVisible();
+
+  await page.reload();
+
+  await expect(page.locator('[name="name"]')).toHaveValue(
+    "Dublin Getaways UPDATED"
+  );
+  await page.locator('[name="name"]').fill("Dublin Getaways");
+  await page.getByRole("button", { name: "Zapisz" }).click();
 });
