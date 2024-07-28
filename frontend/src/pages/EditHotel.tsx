8ba81a5d1c7cd9d3625d7222d057dfc251c9437a
KrzysztofHum <k.humienny1996@protonmail.com>
2024-07-28 19:41:01 +0200

    import showToast in editHotel

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -2,9 +2,11 @@ import { useMutation, useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import * as apiClient from "../api-client";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
 
 const EditHotel = () => {
   const { hotelId } = useParams();
+  const { showToast } = useAppContext();
   const { data: hotel } = useQuery(
     "fetchMyHotelById",
     () => apiClient.fetchMyHotelById(hotelId || ""),
