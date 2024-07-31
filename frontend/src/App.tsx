7c5a44f2a5da2e32432ed5dc711ccb11a36dcd69
KrzysztofHum <k.humienny1996@protonmail.com>
2024-07-31 19:21:16 +0200

    search bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
+import Search from "./pages/Search";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -25,7 +26,14 @@ const App = () => {
             </Layout>
           }
         />
-        <Route path="/search" element={<Layout>Search Page</Layout>} />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <Search />
+            </Layout>
+          }
+        />
         <Route
           path="/register"
           element={
---
+++ frontend/src/pages/Search.tsx
@@ -0,0 +1,8 @@
+import { useSearchContext } from "../contexts/SearchContext"
+
+const Search = () => {
+    const search = useSearchContext();
+
+    return (<>Search Page</>)
+}
+export default Search
